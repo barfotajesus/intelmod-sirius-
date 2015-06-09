0a8c1195a47db9251bb235426d1f0008a16c281d
siriussk8er <siriussk8er@gmail.com>
2015-06-09 09:09:43 +0900

    @4未満 RHS RMH に反応するよう改良

--- IITC- Ingress intel map total conversion.user.js
+++ IITC- Ingress intel map total conversion.user.js
@@ -14964,6 +14964,7 @@ window.renderPortalDetails = function(guid) {
     data = getPortalSummaryData(details);
   }
 
+
   var modDetails = details ? '<div class="mods">'+getModDetails(details)+'</div>' : '';
   var miscDetails = details ? getPortalMiscDetails(guid,details) : '';
   var resoDetails = details ? getResonatorDetails(details) : '';
@@ -15934,7 +15935,6 @@ window.getPortalHackDetails = function(d) {
     var extraHacks = parseInt(mod.stats.BURNOUT_INSULATION);
     numHacks = numHacks + (extraHacks * effectivenessReduction[index]);
   });
-
   return {cooldown: cooldownTime, hacks: numHacks, burnout: cooldownTime*(numHacks-1)};
 }
 
@@ -15954,28 +15954,27 @@ window.getPortalSummaryData = function(d) {
   var maxEnergy = getTotalPortalEnergy(d);
   var curEnergy = getCurrentPortalEnergy(d);
   var health = maxEnergy>0 ? parseInt(curEnergy/maxEnergy*100) : 0;
-
 // @@　こっから下は「しりうす」さんがいじった所 ＃とっても危ないです。
-  var tx
-  var tlvl = " ";
-  var trs = 8;
+// /*
+// var tlvl = " ";
+ var trs = 8;
 //  var d = data;
-  if (d.resonators) {
-    for ( tx in d.resonators) {
-      tlvl += parseInt(d.resonators[tx].level) ;
-      if(d.resonators[tx].level == 8) trs--;
-    }
-  }
-  var tmod = " ";
-  var trar = "";
-  if (d.mods) {
-    for (tx in d.mods) {
-      if (d.mods[tx]) {
-      tmod += d.mods[tx].rarity.capitalize() + d.mods[tx].name ;
+ if (d.resonators) {
+   for (var tx in d.resonators) {
+//     tlvl += parseInt(d.resonators[tx].level) ;
+     if(d.resonators[tx].level == 8) trs--;
+   }
+ }
+ var tmod = " ";
+ var trar = "";
+ if (d.mods) {
+   for (var tx in d.mods) {
+     if (d.mods[tx]) {
+     tmod += d.mods[tx].rarity.capitalize() + d.mods[tx].name ;
 //      tmod += d.mods[tx].name ;
-      }
-    }
-  }
+     }
+   }
+ }
 tmod = tmod.replace(/RareForce Amp/g,"FA/")
 tmod = tmod.replace(/RareTurret/g,"T/")
 tmod = tmod.replace(/RareLink Amp/g,"LA/")
@@ -15987,11 +15986,13 @@ tmod = tmod.replace(/Portal Shield/g,"S/")
 tmod = tmod.replace(/Heat Sink/g,"HS/")
 tmod = tmod.replace(/Multi-hack/g,"MH/")
 var ttext="";
-ttext += d.title + ",L" + d.level + ",@"+trs+"," + tmod + "\n";
+ttext += d.title + ", L" + d.level + ",@"+trs+"," + tmod + "\n";
 //ttext += d.title + "\tL" + d.level + tlvl + tmod + "\n";
-alert(ttext);
+ if (trs < 4 || (ttext.indexOf("RMH") != -1) || (ttext.indexOf("RHS") != -1)) {
+     alert(ttext);
+ }
 //window.clipboardData.setData("text",ttext);
-
+// */
 // @@   　こっから上は「しりうす」さんがいじった所 ＃とっても危ないです。
 
   return {
